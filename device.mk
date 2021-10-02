84609a9538d36c38086c6daa28b34db983f5bccb
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2021-10-02 21:05:01 +0530

    sweet: Enable freeform windows

--- device.mk
+++ device.mk
@@ -182,6 +182,10 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     NexusLauncherRelease \
     CustomPixelLauncherOverlay
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # GPS
 PRODUCT_PACKAGES += \
     libsensorndkbridge
