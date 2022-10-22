008a66caf2f4fa90e5e5464d533e3852ed4afb1a
root <don.boruza@gmail.com>
2022-10-22 17:59:27 +0000

    sweet: Initial rice thirteen

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_sweet.mk
+    $(LOCAL_DIR)/lineage_sweet.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_sweet-eng \
+    lineage_sweet-userdebug \
+    lineage_sweet-user
--- lineage_sweet.mk
+++ lineage_sweet.mk
@@ -11,12 +11,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common PixelExperience stuff
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := false
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit common rice configurations
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := aosp_sweet
+# Other Stuff rice
+RICE_MAINTAINER := ryuKizuha
+RICE_CHIPSET := sm6150
+WITH_GMS := false
+SUSHI_BOOTANIMATION := 1080
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_BUILD_GRAPHENEOS_CAMERA := true
+TARGET_ENABLE_BLUR := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+
+PRODUCT_NAME := lineage_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
