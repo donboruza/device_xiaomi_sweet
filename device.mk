57116268c006a9a5a3c739ad23e402e5ca136ff4
someone5678 <nemui3353@gmail.com>
2022-10-26 22:43:34 +0900

    sweet: Build android.hardware.bluetooth.audio-impl
    10-26 22:35:33.033  6331  6331 W audiohalservice: Could not register Bluetooth Audio Offload API
    10-26 22:35:33.034  6331  6331 E audiohalservice: Failed to dlopen android.hardware.bluetooth.audio-impl.so: dlopen failed: library "android.hardware.bluetooth.audio-impl.so" not found
    10-26 22:35:33.034  6331  6331 W audiohalservice: createIBluetoothAudioProviderFactory() from android.hardware.bluetooth.audio-impl failed

--- device.mk
+++ device.mk
@@ -228,6 +228,7 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
+    android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.bluetooth@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
