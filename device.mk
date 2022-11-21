9d0d5dab8c5ff26ec85dde7335673f6ea2a5f098
root <don.boruza@gmail.com>
2022-11-21 13:39:21 +0000

    sweet: cleanup bluetooth hidl

--- device.mk
+++ device.mk
@@ -229,6 +229,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
@@ -444,10 +445,6 @@ PRODUCT_PACKAGES += \
 # Network
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor
-
-# Bluetooth
-PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0.vendor
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
